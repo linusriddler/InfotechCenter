ea5cb86e87da9f6e1ca58b513e357ffc7bc6926c
Riddle <linusriddler@gmail.com>
2025-02-14 14:06:14 -0500

    Added gasStations Function - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -7,4 +7,10 @@ from time import sleep
 def gasLevelGauge():
   gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
   return random.choice(gasLevelList)
+
+def gasStations():
+    gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "Meijer", "Buc-ees"]
+    return random.choice(gasStationsList)
+
 print(gasLevelGauge())
+print(gasStations())
